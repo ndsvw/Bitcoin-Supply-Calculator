aed1bad8564a98584caf63b3494a9a1405a9aeaa
Alex Lübeck <ndsvw@gmx.de>
2019-11-16 17:40:53 +0100

    changed formatting

--- btcsupply.py
+++ btcsupply.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-def btcSupplyAtBlock(b):
-  reward = 50
-  supply = 0
-  y = 210000 # reward changes all y blocks
-  while b>y:
-    supply = supply + y * reward
-    reward = reward / 2.0
-    b = b - y
-  supply = supply + b * reward
-  return supply
 
-block = 1000000 # you want the supply after which block?
+def btcSupplyAtBlock(b):
+    reward = 50
+    supply = 0
+    y = 210000  # reward changes all y blocks
+    while b > y:
+        supply = supply + y * reward
+        reward = reward / 2.0
+        b = b - y
+    supply = supply + b * reward
+    return supply
+
+
+block = 1000000  # you want the supply after which block?
 print(btcSupplyAtBlock(block))
