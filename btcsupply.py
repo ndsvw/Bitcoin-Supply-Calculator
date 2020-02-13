5deff3d07c0f586db97448b9919bb2fe2c129b86
Alex Lübeck <ndsvw@gmx.de>
2020-02-13 17:52:20 +0100

    calculating on satoshi base (exact integer values instead of decimal values)

--- btcsupply.py
+++ btcsupply.py
@@ -3,15 +3,15 @@
 
 
 def btcSupplyAtBlock(b):
-    reward = 50
+    reward = 50e8
     supply = 0
     y = 210000  # reward changes all y blocks
     while b > y - 1:
         supply = supply + y * reward
-        reward = reward / 2.0
+        reward = int(reward / 2.0)
         b = b - y
     supply = supply + b * reward
-    return supply + reward
+    return (supply + reward) / 1e8
 
 
 block = 1000000  # you want the supply after which block?
