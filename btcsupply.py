f4101cb242d1e2fcddffcad1f3aa01bd9b1df845
Alex Lübeck <ndsvw@gmx.de>
2020-02-13 17:55:41 +0100

    implement that there are only 32 block havings.

--- btcsupply.py
+++ btcsupply.py
@@ -3,15 +3,18 @@
 
 
 def btcSupplyAtBlock(b):
-    reward = 50e8
-    supply = 0
-    y = 210000  # reward changes all y blocks
-    while b > y - 1:
-        supply = supply + y * reward
-        reward = int(reward / 2.0)
-        b = b - y
-    supply = supply + b * reward
-    return (supply + reward) / 1e8
+    if b >= 33 * 210000:
+        return 20999999.9769
+    else:
+        reward = 50e8
+        supply = 0
+        y = 210000  # reward changes all y blocks
+        while b > y - 1:
+            supply = supply + y * reward
+            reward = int(reward / 2.0)
+            b = b - y
+        supply = supply + b * reward
+        return (supply + reward) / 1e8
 
 
 block = 1000000  # you want the supply after which block?
